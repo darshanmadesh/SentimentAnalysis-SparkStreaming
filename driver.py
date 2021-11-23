4b16185ebb8701b27aec6c9f8523abb868c838c9
darshanmadesh <darshan.madesh@gmail.com>
2021-11-23 14:22:39 +0530

    basic SGDClassifier implemented

--- driver.py
+++ driver.py
@@ -10,6 +10,10 @@ import numpy as np
 
 import text_clean as clean
 
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+
 
 def getSparkSessionInstance(sparkConf):
 	if ("sparkSessionSingletonInstance" not in globals()):
@@ -39,16 +43,27 @@ def process(rdd):
 		df = rdd.map(lambda x : (x[0], x[1])).toDF(("target", "text"))
 		lemmatizeDataUDF = udf(clean.lemmatizeData)
 		df = df.withColumn("lemmatized_text", lemmatizeDataUDF(df.text))
-		df.show()
+		#df.show()
 		# collect lemmatized text & target as ndarray
 		sentiment = np.array([int(row['target']) for row in df.collect()])
 		text = np.array([str(row['lemmatized_text']) for row in df.collect()])
 		
+		X_text = vectorizer.transform(text)
+		
+		X_train, X_test, y_train, y_test = train_test_split(X_text, sentiment, test_size=0.15)
+		
+		
+		
+		classifier.partial_fit(X_train, y_train, classes=all_classes)
+		
+		
+		print(classifier.score(X_test, y_test))
 		
 		
 		
 		
-		return rdd
+		
+		#return rdd
 
 		
 #create sparkcontext and the streaming context
@@ -58,6 +73,9 @@ ssc = StreamingContext(sc, 1)
 #read the socket data 
 lines = ssc.socketTextStream("localhost", 6100)
 
+vectorizer = HashingVectorizer( decode_error='ignore', n_features=2**18, alternate_sign=False )
+all_classes = np.array([0, 4])
+classifier = SGDClassifier()
 
 
 lines.foreachRDD(process)
