faa0820dd6aaeb229107035157441031f2e39483
Darshan Madesh <darshan.madesh@gmail.com>
2021-12-03 21:20:35 +0530

    print data sent size

--- stream.py
+++ stream.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 
+import sys
 import time
 import json
 import pickle
@@ -76,10 +77,10 @@ def streamCIFARDataset(tcp_connection, dataset_type='cifar'):
     print("Starting to stream CIFAR data")
     CIFAR_BATCHES = [
         'data_batch_1',
-        # 'data_batch_2',   # uncomment to stream the second training dataset
-        # 'data_batch_3',   # uncomment to stream the third training dataset
-        # 'data_batch_4',   # uncomment to stream the fourth training dataset
-        # 'data_batch_5',    # uncomment to stream the fifth training dataset
+        'data_batch_2',   # uncomment to stream the second training dataset
+        'data_batch_3',   # uncomment to stream the third training dataset
+        'data_batch_4',   # uncomment to stream the fourth training dataset
+        'data_batch_5',    # uncomment to stream the fifth training dataset
         # 'test_batch'      # uncomment to stream the test dataset
     ]
     for batch in CIFAR_BATCHES:
@@ -192,6 +193,7 @@ def streamCSVFile(tcp_connection, input_file):    # stream a CSV file to Spark
         # print(payload)    # uncomment to see the payload being sent
         # encode the payload and add a newline character (do not forget the newline in your dataset)
         send_batch = (json.dumps(payload) + '\n').encode()
+        print(sys.getsizeof(send_batch)/1024)
         try:
             tcp_connection.send(send_batch)  # send the payload to Spark
         except BrokenPipeError:  # this indicates that the message length of the payload is more than what is allowed via TCP
@@ -253,6 +255,8 @@ if __name__ == '__main__':
             _function(tcp_connection, input_file)
     else:
         _function(tcp_connection, input_file)
+
+    tcp_connection.close()
 
 # Setup your own dataset streamer by following the examples above.
 # If you wish to stream a single newline delimited file, use streamFile()
